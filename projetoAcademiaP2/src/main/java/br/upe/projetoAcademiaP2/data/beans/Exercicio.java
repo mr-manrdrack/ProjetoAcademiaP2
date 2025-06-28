ac1525cd3e944635ad62355c6f5a3ae0005a4dc8
Vitoria Riccelle <riccellevitoria@gmail.com>
2025-06-28 11:29:45 -0300

    Fazendo e corrigindo erros nas classes Exercicio e ItemPlanoTreino do beans

--- projetoAcademiaP2/src/main/java/br/upe/projetoAcademiaP2/data/beans/Exercicio.java
+++ projetoAcademiaP2/src/main/java/br/upe/projetoAcademiaP2/data/beans/Exercicio.java
@@ -1,4 +1,41 @@
 package br.upe.projetoAcademiaP2.data.beans;
 
 public class Exercicio {
+
+    private String nome;
+    private String descricao;
+    private String caminhoGif;
+
+    public Exercicio() {
+    }
+
+    public Exercicio(String nome, String descricao, String caminhoGif) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.caminhoGif = caminhoGif;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCaminhoGif() {
+        return caminhoGif;
+    }
+
+    public void setCaminhoGif(String caminhoGif) {
+        this.caminhoGif = caminhoGif;
+    }
 }
--- projetoAcademiaP2/src/main/java/br/upe/projetoAcademiaP2/data/beans/ItemPlanoTreino.java
+++ projetoAcademiaP2/src/main/java/br/upe/projetoAcademiaP2/data/beans/ItemPlanoTreino.java
@@ -1,4 +1,51 @@
 package br.upe.projetoAcademiaP2.data.beans;
 
 public class ItemPlanoTreino {
+
+    private Exercicio exercicio;
+    private int series;
+    private int repeticoes;
+    private int carga;
+
+    public ItemPlanoTreino() {
+    }
+
+    public ItemPlanoTreino(Exercicio exercicio, int series, int repeticoes, int carga) {
+        this.exercicio = exercicio;
+        this.series = series;
+        this.repeticoes = repeticoes;
+        this.carga = carga;
+    }
+
+    public Exercicio getExercicio() {
+        return exercicio;
+    }
+
+    public void setExercicio(Exercicio exercicio) {
+        this.exercicio = exercicio;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getRepeticoes() {
+        return repeticoes;
+    }
+
+    public void setRepeticoes(int repeticoes) {
+        this.repeticoes = repeticoes;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
 }
