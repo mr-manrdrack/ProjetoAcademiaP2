0a225ca31b32c551b29d84085ac175b192a1366b
mr-manrdrack <xavierricardo650@gmail.com>
2025-06-30 18:13:11 -0300

    Add

--- projetoAcademiaP2/src/main/java/br/upe/projetoAcademiaP2/data/beans/Adm.java
+++ projetoAcademiaP2/src/main/java/br/upe/projetoAcademiaP2/data/beans/Adm.java
@@ -7,4 +7,9 @@ public class Adm extends Usuario {
     public Adm(String nome, String telefone, String email, String senha, Double pesoAtual, Double alturaAtual, Double percGorduraAtual) {
 
         super(nome, telefone, email, senha, pesoAtual, alturaAtual, percGorduraAtual);
-    }}
+    }
+
+    public Adm() {
+        super();
+    }
+}
